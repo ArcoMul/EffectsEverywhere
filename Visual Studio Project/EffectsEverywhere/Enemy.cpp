addb22dffd41f92ecf704f9c7a8349bfac8bd582
JGH Rommers <joey.rommers@hva.nl>
2014-03-05 21:51:50 +0100

    Comments added to Enemy.cpp

--- Visual Studio Project/EffectsEverywhere/Enemy.cpp
+++ Visual Studio Project/EffectsEverywhere/Enemy.cpp
@@ -21,13 +21,20 @@ Enemy::Enemy(GameEngine* engine, core::vector3df position)
  */
 void Enemy::addCollision (IMeshSceneNode* collisionNode)
 {
+	// Create an octree triangle selector for collision detection and attach it to our node.
 	ITriangleSelector* selector = _engine->smgr->createOctreeTriangleSelector(node->getMesh(), node, 12);
 	node->setTriangleSelector(selector);
 
+	// Add an animator to the camera, a Collision Response Animator. This animator prevents
+	// your object (player) to move through walls and other objects. The collision box of the enemy
+	// has been set to 7, 7, 7. We do nothing with the gravity, this is why we set the vector to 0, 0, 0.
+	// The last vector is a translation for the animator, which is set to 0, 0, 1.
 	ISceneNodeAnimatorCollisionResponse* collision = _engine->smgr->createCollisionResponseAnimator(
 			selector, collisionNode, core::vector3df(7, 7, 7),
 			core::vector3df(0, 0, 0), core::vector3df(0, 0, 1));
 
+	// We add the animator to our collisionNode and drop the selector and collision if
+	// we don't need it anymore.
 	collisionNode->addAnimator((scene::ISceneNodeAnimator*) collision);
 	selector->drop();
 	collision->drop();
